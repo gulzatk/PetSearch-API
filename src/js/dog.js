8e16aa241ccd02facf87624e2725c90ddd7e4592
Alex Williams and Gulzat Karimova <alex@>
2019-01-16 10:39:11 -0800

    It will update the file to add options to search by breed and show pic randomly

--- src/js/dog.js
+++ src/js/dog.js
@@ -16,4 +16,36 @@ export class Dog {
             request.send();
         });
     }
+
+    getRandomBreedPic(breed) {
+        return new Promise(function(resolve, reject) {
+            let request = new XMLHttpRequest();
+            let url = `https://dog.ceo/api/breed/${breed}/images/random`;
+            request.onload = function() {
+                if (this.status === 200) {
+                    resolve(request.response);
+                } else {
+                    reject(Error(request.statusText));
+                }
+            }
+            request.open("GET", url, true);
+            request.send();
+        });
+    }
+
+    getBreedList() {
+        return new Promise(function(resolve, reject) {
+            let request = new XMLHttpRequest();
+            let url = "https://dog.ceo/api/breeds/list/all";
+            request.onload = function() {
+                if (this.status === 200) {
+                    resolve(request.response);
+                } else {
+                    reject(Error(request.statusText));
+                }
+            }
+            request.open("GET", url, true);
+            request.send();
+        });
+    }
 }
--- src/main.js
+++ src/main.js
@@ -5,19 +5,47 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Dog } from './js/dog';
 
 
+let dogPic = new Dog();
+let promise = dogPic.getBreedList();
+
+
+
+promise.then(function(response) {
+    let body = JSON.parse(response);
+    let list = body.message;
+    const selectForm = $("#breedList");
+    console.log(selectForm);
+    console.log(list);
+    for(let breed in list){
+        selectForm.append(`<option value = "${breed}">${breed}</option>`);
+    }
+});
 
 
 $(document).ready(function () {
-    $("#userForm").submit(function(event) {
+    $("#randomDog").submit(function(event) {
         event.preventDefault();
-       let dogPic = new Dog();
-       let promise = dogPic.getRandomDogPic();
+        let dogPic = new Dog();
+        let promise = dogPic.getRandomDogPic();
 
-       promise.then(function(response) {
-           let body = JSON.parse(response);
-           let img = new Image();
-           img.src = body.message;
-           document.getElementById("dogImgOutput").src = img.src;
-       })
+        promise.then(function(response) {
+            let body = JSON.parse(response);
+            let img = new Image();
+            img.src = body.message;
+            document.getElementById("dogImgOutput").src = img.src;
+        })
+    });
+    $("#randomBreed").submit(function(event) {
+        event.preventDefault();
+        let breed = $("#breedList").val();
+        let dogPic = new Dog();
+        let promise = dogPic.getRandomBreedPic(breed);
+
+        promise.then(function(response) {
+            let body = JSON.parse(response);
+            let img = new Image();
+            img.src = body.message;
+            document.getElementById("dogBreedImgOutput").src = img.src;
+        })
     });
 });
